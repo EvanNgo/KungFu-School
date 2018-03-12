706a271af0b46568fdbf90e2a9bed6207c740b50
Evan <evanngo93@gmail.com>
2018-03-12 19:14:51 +0900

    add first respawn enemy

---
+++ KungFuSchool/Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    //spawn aliens. This would go on a script called GameManager on empty GameObject or something
+    public GameObject enemy;
+    public float CountDown = 3f;
+    public bool Dead = false;
+    float Timer = 0;
+    void Start(){
+        SpawnEnemy(); //example of calling a function from within the script
+    }
+
+    void Update(){
+        if (Dead)
+        {
+            Timer += Time.deltaTime;
+            if (Timer > CountDown)
+            {
+                SpawnEnemy();
+            }
+        }
+    }
+
+    public void SpawnEnemy(){
+        //spawns alien gameObject at spawnPoint position, and give it a rotation to complete the function
+        Instantiate(enemy,transform.position, Quaternion.identity);
+        Dead = false;
+        Timer = 0;
+    }
+}
